26743c4e002369e06c9e58e3200806ba28551d2f
Slawomir Potoczek <plgdestrim@login02.prometheus.cyfronet.pl>
2022-04-26 16:00:10 +0200

    add res for small and big problem

--- lab2/runBigProblem.sh
+++ lab2/runBigProblem.sh
@@ -2,11 +2,14 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
 #SBATCH --sockets-per-node=2
-#SBATCH --time=00:10:00
+#SBATCH --time=00:03:00
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
-size=100000000
+size=150000000
 
 module add plgrid/tools/openmpi
+module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
+
 mpiexec -np 12 ./approx_pi.py $size >> res_big_problem.csv
+
