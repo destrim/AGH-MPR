5b925434e9c6efcead38adcd68f828fc90db463c
Sławomir Potoczek <slawomir.potoczek@comarch.pl>
2022-04-27 08:53:45 +0200

    add script for plgrid-short

---
+++ lab2/runPromOneIter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 12
+#SBATCH --sockets-per-node=2
+#SBATCH --time=00:20:00
+#SBATCH --partition=plgrid-short
+#SBATCH --account=plgmpr22
+
+size=10000,1224745,150000000
+
+module add plgrid/tools/openmpi
+module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
+
+for s in ${size//,/ }
+do
+  for th in {1..12}
+  do
+    mpiexec -np $th ./approx_pi.py $s >> res_all.csv
+  done
+done
