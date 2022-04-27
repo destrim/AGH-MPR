3187de6b09f0828326603c6812bd45f40b9169ec
Sławomir Potoczek <slawomir.potoczek@comarch.pl>
2022-04-27 10:19:45 +0200

    change scripts

--- lab2/runB.sh
+++ lab2/runB.sh
@@ -2,19 +2,16 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
 #SBATCH --sockets-per-node=2
-#SBATCH --time=03:00:00
-#SBATCH --partition=plgrid
+#SBATCH --time=00:20:00
+#SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
-size=150000000
+size=200000000
 
 module add plgrid/tools/openmpi
 module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
 
-for i in {1..10}
+for th in {1..12}
 do
-    for th in {1..12}
-    do
-      mpiexec -np $th ./approx_pi.py $size >> res_big.csv
-    done
+  mpiexec -np $th ./approx_pi.py $size >> res_big.csv
 done
