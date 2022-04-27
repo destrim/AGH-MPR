8a5648498750957311cff08487b48ea5ac5be0ad
Sławomir Potoczek <slawomir.potoczek@comarch.pl>
2022-04-27 13:28:51 +0200

    changes

--- lab2/runBigProblem.sh
+++ lab2/runBigProblem.sh
@@ -6,9 +6,11 @@
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgmpr22
 
-size=300000000
-
 module add plgrid/tools/openmpi
 module add plgrid/libs/python-mpi4py/3.0.0-python-2.7
 
-mpiexec -np 12 ./approx_pi.py $size >> res_big_problem.csv
+for i in {1..9}
+do
+  size=$((100000000 * i))
+  mpiexec -np 12 ./approx_pi.py $size >> search_big.csv
+done
